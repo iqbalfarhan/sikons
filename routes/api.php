d1b3abb33cd5de6e7a4842e91f376bd6dada75e1
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-02-22 16:00:58 +0800

    udpate ap

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,10 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', [AuthController::class, 'user']);
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/laporan/mine', [LaporanController::class, 'mine']);
-
+    Route::post('/laporan/generate', [LaporanController::class, 'generate']);
     Route::resource('laporan', LaporanController::class);
 });
 
-Route::post('/laporan/generate', [LaporanController::class, 'generate']);
 
 Route::post('/login',  [AuthController::class,'login']);
 
